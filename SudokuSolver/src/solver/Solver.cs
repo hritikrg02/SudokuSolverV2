e2b4891614fab0617d437ddcb58f5415d791dbf5
Ricky Gupta <rg4825@rit.edu>
2022-01-22 17:18:28 -0500

    Added documentation to Solver.cs

--- SudokuSolver/src/solver/Solver.cs
+++ SudokuSolver/src/solver/Solver.cs
@@ -7,13 +7,24 @@ namespace SudokuSolver.Solver;
 /// Attempts to find a solution to a puzzle using a DFS algorithm.
 /// </summary>
 public class Solver {
+    /// <summary>
+    /// Number of configs that have been generated.
+    /// </summary>
     private long _numConfigs = 0;
-
+    
+    /// <summary>
+    /// Property for the number of configs that have been generated.
+    /// </summary>
     public long NumConfigs {
         get => _numConfigs;
         private set => _numConfigs = value;
     }
-
+    
+    /// <summary>
+    /// Uses a recursive backtracker to solve a given config.
+    /// </summary>
+    /// <param name="config">A configuration of a puzzle</param>
+    /// <returns></returns>
     public List<IConfiguration> Solve(IConfiguration config) {
         if (config.IsSolution()) {
             var optional = new List<IConfiguration>();
